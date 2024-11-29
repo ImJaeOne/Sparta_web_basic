2222f78f3e1cf59483bcb1053d41a4174c5cd247
Jaewon Lim <dlawi0108@naver.com>
2024-11-29 16:22:20 +0900

    daily team study

---
+++ teamStudy/react_devtools_backend_compact.js
@@ -0,0 +1,12 @@
+console.time('그리는 데 걸린 시간: ');
+console.log(`
+        /\\___
+        (   @\\___
+        /        0
+        /  (____/
+        /____/ U
+    `);
+console.log('이것은 간단한 ASCII 아트 강아지입니다.');
+console.warn('이 강아지는 그림입니다. 실제 강아지가 아닙니다!');
+console.error(`오류: 실제 강아지 데이터를 불러오지 못했습니다.`);
+console.timeEnd('그리는 데 걸린 시간: ');
