79fd91a97a32053edd763dbda42e7226bd98f9ff
Jaewon Lim <dlawi0108@naver.com>
2024-11-30 20:51:23 +0900

    spartaflix jquery

--- spartaflix/index.js
+++ spartaflix/index.js
@@ -1,6 +1,27 @@
-const recordBtn = document.getElementsByClassName('btn-outline-light')[1];
+$('.btn-outline-light')
+    .eq(1)
+    .on('click', function () {
+        $('#postingbox').toggle();
+    });
 
-recordBtn.addEventListener('click', function () {
-    alert('안녕하세요.');
-    $('#title').text('쥬라기월드');
-});
+$('.btn-danger')
+    .eq(1)
+    .on('click', function () {
+        let image = $('#image').val();
+        let title = $('#title').val();
+        let star = $('#star').val();
+        let comment = $('#comment').val();
+        let temp_html = `
+    <div class="col">
+                <div class="card h-100">
+                    <img src="${image}"
+                        class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <p class="card-text">${star}</p>
+                        <p class="card-text">${comment}</p>
+                    </div>
+                </div>
+            </div>`;
+        $('#card').append(temp_html);
+    });
