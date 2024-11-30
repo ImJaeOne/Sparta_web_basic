1ee9be6061b8659b1f85d897befdf5508da680e0
Jaewon Lim <dlawi0108@naver.com>
2024-11-30 15:09:37 +0900

    album jquery

---
+++ album/index.js
@@ -0,0 +1,28 @@
+$('#remember_btn').on('click', function () {
+    $('#postingbox').toggle();
+});
+
+$('#record_btn').on('click', function () {
+    let image = $('#image').val();
+    let title = $('#title').val();
+    let content = $('#content').val();
+    let date = $('#date').val();
+
+    let temp_html = `
+    <div class="col">
+                <div class="card h-100">
+                    <img src="${image}"
+                        class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <p class="card-text">${content}</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-body-secondary">${date}</small>
+                    </div>
+                </div>
+            </div>`;
+    $('#card').append(temp_html);
+});
+
+//우리가 만든 카드는 브라우저에 저장된거기 때문에 새로고침하면 날라감 => 서버의 필요성
